d9098e39439b85ab6cc420b3f01466bb94e13795
ahmad123r <ahmadzero___@hotmail.com>
2021-07-13 14:32:28 +0300

    First two project

---
+++ React/fist/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div id="root">
+            <h1>Hello Dojo!</h1>
+            <h2>Things i want to do :</h2>
+            <ul>
+              <li>Learn React</li>
+              <li>Climb mr Evaerst</li>
+              <li> run a marathon</li>
+              <li>feeds the dogs</li>
+
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+
+export default App;
